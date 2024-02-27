088a5d52aa04fcc1597183945bd14b29c0a00396
GhostplayerMusic <ghostplayermusic@gmail.com>
2024-02-27 15:47:06 -0500

    Update Feldman_mice.py
    Corrected

--- Feldman_mice.py
+++ Feldman_mice.py
@@ -1,1 +1,7 @@
-print("        3\n          blind\n               mice")
+#Write a Python program (named {yourfamilyname}mice.py) 
+#that prints out the following lines using only one 
+#print function and where the number 3 is a value 
+#(the result of a calculation):
+
+a = 5-2
+print(f"        {a}\n          blind\n               mice")
